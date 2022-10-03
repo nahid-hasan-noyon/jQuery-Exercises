aa3a9d168a1f085a3c6688cb147b2919e1e05a12
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-03 23:11:02 +0600

    learning css selector on single id, class, element,

---
+++ js/script-2.js
@@ -0,0 +1,11 @@
+$(function () {
+	// * CSS selector
+	// on element
+	$('p').css('background-color', 'rgba(180,180,30,0.8');
+	// on class
+	$('.red-box').css('background-color', 'rgba(180,180,30,0.8');
+	// on id
+	$('#list').css('background-color', 'rgba(180,180,30,0.8');
+	// specify by type of and element
+	$('input[type="text"]').css('background-color', 'rgba(180,180,30,0.8');
+});
