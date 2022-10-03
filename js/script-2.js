86348b05d0acd7889a1b13a844ae181f9294afc2
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-03 23:12:56 +0600

    learning css selector on multiple elements at once

--- js/script-2.js
+++ js/script-2.js
@@ -1,11 +1,13 @@
 $(function () {
 	// * CSS selector
-	// on element
-	$('p').css('background-color', 'rgba(180,180,30,0.8');
-	// on class
-	$('.red-box').css('background-color', 'rgba(180,180,30,0.8');
-	// on id
-	$('#list').css('background-color', 'rgba(180,180,30,0.8');
-	// specify by type of and element
-	$('input[type="text"]').css('background-color', 'rgba(180,180,30,0.8');
+	// // on element
+	// $('p').css('background-color', 'rgba(180,180,30,0.8');
+	// // on class
+	// $('.red-box').css('background-color', 'rgba(180,180,30,0.8');
+	// // on id
+	// $('#list').css('background-color', 'rgba(180,180,30,0.8');
+	// // specify by type of and element
+	// $('input[type="text"]').css('background-color', 'rgba(180,180,30,0.8');
+	// * css selector on multiple elements at once
+	$('h2, p, input, #list').css('background-color', 'rgba(180,180,30,0.8');
 });
