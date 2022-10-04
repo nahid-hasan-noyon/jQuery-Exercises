7d091d9c71d7483773e301c518568c39f5a2d699
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-04 11:58:27 +0600

    learning to traves with jQuery

--- js/script-2.js
+++ js/script-2.js
@@ -13,5 +13,22 @@ $(function () {
 	// * css selector on child element
 	// $('p:first').css('background-color', 'rgba(180,180,30,0.8');
 	// * css selector on multiple child element at once
-	$('li:odd, input:text ').css('background-color', 'rgba(180,180,30,0.8');
+	// $('li:odd, input:text ').css('background-color', 'rgba(180,180,30,0.8');
+	// * Traversing with jQuery
+	// $('#list').find('li').css('background-color', 'rgba(180,180,30,0.8');
+	// ? find will access all the li inside list
+	// $('#list').children('li').css('background-color', 'rgba(180,180,30,0.8');
+	// ? children will select the direct descendant of list
+	// $('#list').parent().css('background-color', 'rgba(180,180,30,0.8');
+	// ? parent will select the direct ancestor of list
+	// $('#list').parents('body').css('background-color', 'rgba(180,180,30,0.8');
+	// ? parents will select the any or specific ancestor of list
+	// $('#list').siblings('h2, p').css('background-color', 'rgba(180,180,30,0.8');
+	// ? siblings are those who are in same level or indention with list
+	// $('#list').siblings(':header').css('background-color', 'rgba(180,180,30,0.8');
+	// ?  it is a special case
+	// $('#list').prev().css('background-color', 'rgba(180,180,30,0.8');
+	// ? prev will selcet the previous element
+	// $('#list').next().css('background-color', 'rgba(180,180,30,0.8');
+	// ? next will selcet the next element
 });
