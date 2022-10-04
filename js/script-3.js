3ed3bd0c2c91c8331512b345ab3b38ce2f2aee66
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-04 21:19:16 +0600

    learned to add new element to the DOM at the first of an element using prepend and prependTo

--- js/script-3.js
+++ js/script-3.js
@@ -1,7 +1,10 @@
 $(function () {
 	// * Adding new element to the DOM
-	$('ul ul').append('<li>This is append method</li>');
-	// ? adding an li inside of an ul which inside of another ul
-	$('<li>This is append method</li>').appendTo('ul ul');
+	// $('ul ul').append('<li>This is append method</li>');
+	// ? adding an li inside the end of an ul which inside of another ul
+	// $('<li>This is appendTo method</li>').appendTo('ul ul');
 	// ? alternative of append
+	$('ul ul').prepend('<li>This is prepend method</li>');
+	// ? adding an li inside the first of an ul which is inside of another ul
+	$('<li>This is prependTo method</li>').prependTo('ul ul');
 });
