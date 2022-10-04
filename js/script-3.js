85684cd59352068184da7a40d13c5b83480e3824
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-04 21:24:36 +0600

    learned to add element to the DOM using after and before.

--- js/script-3.js
+++ js/script-3.js
@@ -4,7 +4,11 @@ $(function () {
 	// ? adding an li inside the end of an ul which inside of another ul
 	// $('<li>This is appendTo method</li>').appendTo('ul ul');
 	// ? alternative of append
-	$('ul ul').prepend('<li>This is prepend method</li>');
+	// $('ul ul').prepend('<li>This is prepend method</li>');
 	// ? adding an li inside the first of an ul which is inside of another ul
-	$('<li>This is prependTo method</li>').prependTo('ul ul');
+	// $('<li>This is prependTo method</li>').prependTo('ul ul');
+	// * adding element after an element
+	$('.red-box').after('<div class="red-box">After Red</div>');
+	// * adding element before an element
+	$('.blue-box').before('<div class="blue-box">Before Blue</div>');
 });
