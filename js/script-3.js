3092c08728cee0aa7afc91b585f816b02ca93003
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-04 21:30:07 +0600

    learned to use function inside before method.

--- js/script-3.js
+++ js/script-3.js
@@ -8,7 +8,11 @@ $(function () {
 	// ? adding an li inside the first of an ul which is inside of another ul
 	// $('<li>This is prependTo method</li>').prependTo('ul ul');
 	// * adding element after an element
-	$('.red-box').after('<div class="red-box">After Red</div>');
+	// $('.red-box').after('<div class="red-box">After Red</div>');
 	// * adding element before an element
-	$('.blue-box').before('<div class="blue-box">Before Blue</div>');
+	// $('.blue-box').before('<div class="blue-box">Before Blue</div>');
+	// * adding new element using function in before
+	$('.blue-box').before(function () {
+		return '<div class="blue-box">Before Blue</div>';
+	});
 });
