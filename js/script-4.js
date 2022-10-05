8aef3a85e4dc7e06316493a9d3915b12cb2872af
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-05 23:40:09 +0600

    learned to hide an element using css(), hide() method and also show it using show() method.

--- js/script-4.js
+++ js/script-4.js
@@ -1,1 +1,9 @@
-$(function () {});
+$(function () {
+	// * Changing the css Property of an Element
+	// $('.gallery').css('display', 'none');
+	// ? changing property by giving the property name then the value
+	// $('.gallery').hide();
+	// ? alternative way to hide the element
+	// $('.gallery').show();
+	// ? Showing a hidden or display none element
+});
