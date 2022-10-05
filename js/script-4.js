a3564d766ee08553fba3249a4f40f488b878f64f
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-05 23:47:33 +0600

    learned to change css properties

--- js/script-4.js
+++ js/script-4.js
@@ -12,5 +12,21 @@ $(function () {
 	// ? getting the width of the red box
 	console.log(redBox.width());
 	// ? alternative way to get the width of the red box
-	// ! the width() method is not the same as the css() method because the css() method returns the width with units (px, em, etc)
+	// ! the width() method is not the same as the css() method because the css() method returns the width with units (px, em, etc) while the width() method returns the width without units, the width() method is a getter method while the css() method is a setter method
+	redBox.css('background-color', '#AA7700');
+	// ? changing the background color of the red box
+	$('p').css('font-size', '18px');
+	// ? changing the font size of all the paragraphs
+	$('p').css('font-size', '+=2px');
+	// ? increasing the font size of all the paragraphs by 2px
+	var properties = $('p').css(['font-size', 'line-height', 'color']);
+	// ? getting the font size, line height, and color of all the paragraphs
+	console.log(properties);
+	console.log(properties['font-size']);
+	// ? getting the font size of all the paragraphs
+	redBox.css('user-select', 'none');
+	// ? disabling the user from selecting the text in the red box
+	redBox.css('user-select', function () {
+		return 'none';
+	});
 });
