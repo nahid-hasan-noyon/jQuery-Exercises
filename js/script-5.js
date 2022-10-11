43142add531dcb019639ae49fd69ea6e88ddcfff
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-12 00:20:10 +0600

    Completed the 16th Challenge.

--- js/script-5.js
+++ js/script-5.js
@@ -5,4 +5,10 @@ $(function () {
 		// event is an object that contains information about the event
 		alert('Button Clicked');
 	});
+	// * 16th Challenge
+	$('.red-box').click(function (e) {
+		$('.red-box').fadeTo(1000, 0.5);
+	});
+	$('.red-box').click();
+	// ? The above line will trigger the click event on the red box on page load
 });
