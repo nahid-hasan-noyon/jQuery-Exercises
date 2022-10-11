fceafaafc856862e82f82515736004f7a97c2c8a
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-12 00:25:06 +0600

    Learned the hover method

--- js/script-5.js
+++ js/script-5.js
@@ -1,14 +1,19 @@
 $(function () {
 	// * Adding click handler
-	$('#btn-click').click(function (event) {
-		console.log(event);
-		// event is an object that contains information about the event
-		alert('Button Clicked');
-	});
+	// $('#btn-click').click(function (event) {
+	// 	console.log(event);
+	// ? event is an object that contains information about the event
+	// 	alert('Button Clicked');
+	// });
 	// * 16th Challenge
-	$('.red-box').click(function (e) {
-		$('.red-box').fadeTo(1000, 0.5);
-	});
-	$('.red-box').click();
+	// $('.red-box').click(function (e) {
+	// 	$('.red-box').fadeTo(1000, 0.5);
+	// });
+	// $('.red-box').click();
 	// ? The above line will trigger the click event on the red box on page load
+	// * Adding Hover Handler
+	// $('.green-box').hover(function () {
+	// 	alert('Hovered');
+	// });
+	// ! using hover is not a good idea as it will trigger the event twice
 });
