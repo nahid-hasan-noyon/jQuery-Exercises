2fb821d9b5d3e6f9c050a6768e1fbce20a0f62a6
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-13 00:37:57 +0600

    learned to load a file in local storage

--- js/script-7.js
+++ js/script-7.js
@@ -1,1 +1,16 @@
-$(function () {});
+$(function () {
+	// * Fetch the file from the server
+	// $('#code').load('text.txt');
+	// * Fetch the file from the server with a callback function
+	// $('#code').load('js/script.js', function (response, status) {
+	// 	if (status == 'error') {
+	// 		alert('Error: ' + response);
+	// 	}
+	// });
+	// ? unavailable file will give an error
+	// $('#code').load('text.txt', function (response, status) {
+	// 	if (status == 'error') {
+	// 		alert('Error: ' + response);
+	// 	}
+	// });
+});
