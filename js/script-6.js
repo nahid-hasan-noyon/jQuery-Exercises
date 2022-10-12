5a0c4ba619531033de17b2fde64d729c2bfc8bfa
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-12 22:18:16 +0600

    learned to use focus and blur method

--- js/script-6.js
+++ js/script-6.js
@@ -1,1 +1,10 @@
-$(function () {});
+$(function () {
+	// * The Focus and Blur Events
+	var inputFields = $('input:text, input:password, textarea');
+	inputFields.focus(function () {
+		$(this).css('box-shadow', '0 0 5px #666');
+	});
+	inputFields.blur(function () {
+		$(this).css('box-shadow', 'none');
+	});
+});
