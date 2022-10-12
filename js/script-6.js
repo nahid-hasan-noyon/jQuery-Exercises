98e84c4a1735dd1a4e486f24d7c882dc34583dea
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-12 22:55:29 +0600

    learned the change event method

--- js/script-6.js
+++ js/script-6.js
@@ -1,20 +1,30 @@
 $(function () {
 	// * The Focus and Blur Events
-	var inputFields = $('input:text, input:password, textarea');
-	inputFields.focus(function () {
-		$(this).css('box-shadow', '0 0 5px #666');
-	});
-	inputFields.blur(function () {
-		$(this).css('box-shadow', 'none');
-	});
+	// var inputFields = $('input:text, input:password, textarea');
+	// inputFields.focus(function () {
+	// 	$(this).css('box-shadow', '0 0 5px #666');
+	// });
+	// inputFields.blur(function () {
+	// 	$(this).css('box-shadow', 'none');
+	// });
 	// * 24th Challenge
-	$('#name').blur(function () {
-		var name = $(this).val();
-		console.log(name);
-		if (name.length < 4) {
-			$(this).css('box-shadow', '0 0 10px #811');
-		} else {
+	// $('#name').blur(function () {
+	// 	var name = $(this).val();
+	// 	console.log(name);
+	// 	if (name.length < 4) {
+	// 		$(this).css('box-shadow', '0 0 10px #811');
+	// 	} else {
+	// 		$(this).css('box-shadow', '0 0 5px #181');
+	// 	}
+	// });
+	////
+	// * The Change Event
+	$('#cb').change(function () {
+		var isChecked = $(this).is(':checked');
+		if (isChecked) {
 			$(this).css('box-shadow', '0 0 5px #181');
+		} else {
+			$(this).css('box-shadow', '0 0 5px #811');
 		}
 	});
 });
