8f7a8669a20181ccba61342351a47049efc6a5a9
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-13 12:50:20 +0600

    learned to access pokeAPI data

--- js/script-7.js
+++ js/script-7.js
@@ -15,22 +15,35 @@ $(function () {
 	// });
 	////
 	// * Retrieve Flicker images from Flicker API
-	var flickerAPI =
-		'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
-	$.getJSON(flickerAPI, {
-		tags: 'cat',
-		tagmode: 'any',
-		format: 'json',
-	})
-		.done(function (data) {
-			$.each(data.items, function (index, item) {
-				$('<img>').attr('src', item.media.m).appendTo('#flickrImages');
-				if (index == 4) {
-					return false;
-				}
-			});
-		})
-		.fail(function (jqXHR, textStatus, errorThrown) {
-			alert('Error: ' + errorThrown);
+	// var flickerAPI =
+	// 	'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+	// $.getJSON(flickerAPI, {
+	// 	tags: 'cat',
+	// 	tagmode: 'any',
+	// 	format: 'json',
+	// })
+	// 	.done(function (data) {
+	// 		$.each(data.items, function (index, item) {
+	// 			$('<img>').attr('src', item.media.m).appendTo('#flickrImages');
+	// 			if (index == 4) {
+	// 				return false;
+	// 			}
+	// 		});
+	// 	})
+	// 	.fail(function (jqXHR, textStatus, errorThrown) {
+	// 		alert('Error: ' + errorThrown);
+	// 	});
+	////
+	// * Retrieving pokemon data from the pokeAPI
+	var pokeAPI = 'https://pokeapi.co/api/v2/pokemon/';
+	$.getJSON(pokeAPI).done(function (data) {
+		// console.log(data);
+		$.each(data.results, function (index) {
+			var pokeName =
+				this.name.charAt(0).toUpperCase() + this.name.slice(1);
+			$('<li>')
+				.text('Pokemon no.' + ++index + ' ' + pokeName)
+				.appendTo('#pokemonContainer');
 		});
+	});
 });
