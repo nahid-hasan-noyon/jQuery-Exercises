505185ee2cfc5c7d787942c2f14f45e6769a6f73
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-13 12:25:51 +0600

    learned to retrieve images and show it on the page from flickr api

--- js/script-7.js
+++ js/script-7.js
@@ -13,4 +13,24 @@ $(function () {
 	// 		alert('Error: ' + response);
 	// 	}
 	// });
+	////
+	// * Retrieve Flicker images from Flicker API
+	var flickerAPI =
+		'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+	$.getJSON(flickerAPI, {
+		tags: 'cat',
+		tagmode: 'any',
+		format: 'json',
+	})
+		.done(function (data) {
+			$.each(data.items, function (index, item) {
+				$('<img>').attr('src', item.media.m).appendTo('#flickrImages');
+				if (index == 4) {
+					return false;
+				}
+			});
+		})
+		.fail(function (jqXHR, textStatus, errorThrown) {
+			alert('Error: ' + errorThrown);
+		});
 });
